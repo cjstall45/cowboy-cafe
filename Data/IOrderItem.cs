7c49827226af8d785b17f2400d7b497483bbed1b
cjstall45 <cjstall45@F1092-21-2109>
2020-02-26 14:49:23 -0600

    finish tests

---
+++ Data/IOrderItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// An interface representing a single item in an order
+    /// </summary>
+    public interface IOrderItem
+    {
+        /// <summary>
+        /// the price of the order item
+        /// </summary>
+        double Price { get; }
+        /// <summary>
+        /// the special instructions for the order item
+        /// </summary>
+        List<string> SpecialInstructions { get; }
+    }
+}
