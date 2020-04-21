207b38213a10a2f38318c0d8bc649fdc351cffa7
Charlie Stallbaumer <cjstall45@ksu.edu>
2020-04-20 20:35:58 -0500

    working on website milestone 1

---
+++ Data/Menu.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    public static class Menu
+    {
+        public static IEnumerable<IOrderItem> Entrees()
+        {
+            List<IOrderItem> entree = new List<IOrderItem>();
+
+        }
+
+        public static IEnumerable<IOrderItem> Sides()
+        {
+
+        }
+
+        public static void SideSize(Side side, Size s)
+        {
+            side.Size = s;
+
+        }
+
+        public static void DrinkSize(Drink drink, Size s)
+        {
+            drink.Size = s;
+        }
+
+    }
+}
